d983b94dd793dcea7de190cde91dec550bc1b05d
azure4-mac <137010606+azure4-mac@users.noreply.github.com>
2025-05-18 13:47:34 -0300

    Update main.sql

--- main.sql
+++ main.sql
@@ -1,5 +1,4 @@
--- SQLBook: Code
--- Inserir um hieróglifo A1
+-- Inserir um hieróglifo no banco de dados
 INSERT INTO hieroglyph (
     gardiner_code, unicode_code, description, ideogram, notes
 ) VALUES (
