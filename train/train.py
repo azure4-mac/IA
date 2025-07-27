461c206e7f27dffbf94c3407c14f3b1a4067779f
Jailson Stein <steinjailson@gmail.com>
2025-07-27 12:24:15 -0300

    feat: output

--- train/train.py
+++ train/train.py
@@ -2,7 +2,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-num_classes = 402  
+num_classes = 10
 
 model = Sequential([
     Conv2D(32, (3,3), activation='relu', input_shape=(64, 64, 3)),
@@ -15,7 +15,7 @@ model = Sequential([
 ])
 
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
+    
 datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
 
 train_gen = datagen.flow_from_directory(
